8eb5840f49641bd9646e3b17b4b28688795825f9
dilamd <dnnawarathna01@gmail.com>
2025-03-30 07:53:04 +0530

    Update session.js

--- session.js
+++ session.js
@@ -40,8 +40,8 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your session id here",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~bV0iUDgB#ElpdTcz5ABlmpWF6lunVO5V1qq7RxwrHH4s_8Ehg4no",
 SUDO: process.env.SUDO || ["94717775628","94758775628"],
 PORT: process.env.PORT || "8000",
-FOOTER: process.env.FOOTER || "> QUEEN ANJU XPRO\n> Developed by Mr. Rashmika\n> GitHub Repository: github.com/Mrrashmika/QUEEN_ANJU_xPro\n🌟 Premium Version | Advanced Features | Secure & Reliable 🌟\n© 2025 QUEEN ANJU XPRO. All rights reserved.",
+FOOTER: process.env.FOOTER || "> 📥 ᵈᵒʷⁿˡᵒᵃᵈ ʷⁱᵗʰⁱⁿᵍ 14 ᵈᵃʸˢ.❗ \ > *`✨️𝘿𝙆 𝙈𝙊𝙑𝙄𝙀𝙎✨️`* \ *❖◉◎__[D̲̅][I̲̅][L̲̅][A̲̅]__◎◉❖*",
 };
